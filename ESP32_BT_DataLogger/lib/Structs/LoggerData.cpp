93d397d4872d17f9bff350ee6e04c1d1a1953b58
David <simanek@email.cz>
2021-10-12 00:27:34 +0200

    deep sleep with wake up from RTC to log data OR Button for BT mode

--- ESP32_BT_DataLogger/lib/Structs/LoggerData.cpp
+++ ESP32_BT_DataLogger/lib/Structs/LoggerData.cpp
@@ -7,13 +7,7 @@ String GetLoggerDataAsString(pLoggerData data)
     DateTime d = DateTime(data->Time);
     char buffer[21];
     sprintf(buffer, "%02d.%02d.%04d %02d:%02d:%02d", d.day(), d.month(), d.year(), d.hour(), d.minute(), d.second());
-    // String date =
-    //     String(d.day()) + "." +
-    //     String(d.month()) + "." +
-    //     String(d.year()) + " " +
-    //     String(d.hour()) + ":" +
-    //     String(d.minute()) + ":" +
-    //     String(d.second());
+    
     return "Temperature;" + String(data->Temperature / 10.0) +
            ";[°C];Humidity;" + String(data->Humidity / 10.0) +
            ";[%];Date;" + String(buffer);
